52a14fa693314800e9ccd425f381dabc2b1811a2
Brian Leip <brianleip@gmail.com>
2020-04-30 01:27:31 -0700

    Created fade in and out UI transitions

---
+++ Eye-Catching-UI/Assets/Transitions/BLTransitions/Fade2.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Fade2 : MonoBehaviour
+{
+    Animator anim;
+
+    private void Start() {
+        anim = GetComponent<Animator>();    
+    }
+
+    // Update is called once per frame
+    void Update() {
+
+        // toggles paused screen on spacebar
+        if (Input.GetKeyUp(KeyCode.Space) ) {
+            bool isFaded = anim.GetBool("fade");
+            if (!isFaded)
+                anim.SetBool("fade", true); 
+            else
+                anim.SetBool("fade", false);
+        }
+    }
+}
---
+++ Eye-Catching-UI/Assets/Transitions/BLTransitions/Pause.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pause : MonoBehaviour
+{
+    Animator anim;
+
+    private void Start() {
+        anim = GetComponent<Animator>();    
+    }
+
+    // Update is called once per frame
+    void Update() {
+
+        // toggles paused screen on spacebar
+        if (Input.GetKeyUp(KeyCode.P) ) {
+            bool isPaused = anim.GetBool("paused");
+            if (!isPaused)
+                anim.SetBool("paused", true); 
+            else
+                anim.SetBool("paused", false);
+        }
+    }
+}
